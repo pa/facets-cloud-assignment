ae3b13d4df330a3b021e52f8044f880c49ff792c
pa <pramodh.ayyappan92@gmail.com>
2022-06-17 02:38:00 +0530

    Updated task two read me with deployment instructions

--- task-two--strech-goal/examples/main.tf
+++ task-two--strech-goal/examples/main.tf
@@ -1,6 +1,6 @@
 module "terraform-module-k8s-bg-dply" {
     source = "../"
     apps_json_file_path = "applications.json"
-    external_ips = ["192.168.64.5"]
-    host_name = "example.com"
+    external_ips = ["<ip-address>"]
+    host_name = "<host-name>"
 }
